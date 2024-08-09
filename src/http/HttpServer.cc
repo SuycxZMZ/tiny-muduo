d62daebb174b0b748807e3aedb40dee52914edae
SuycxZMZ <suycx65@163.com>
2024-08-09 16:41:34 +0800

    httpserver加一点注释

--- src/http/HttpServer.cc
+++ src/http/HttpServer.cc
@@ -61,11 +61,11 @@ void HttpServer::onMessage(const TcpConnectionPtr& conn,
     std::cout << request << std::endl;
 #endif
 
-    // 进行状态机解析
+    // 进行 状态机解析
     // 错误则发送 BAD REQUEST 半关闭
     if (!context->parseRequest(buf, receiveTime))
     {
-        LOG_INFO << "parseRequest failed!";
+        LOG_ERROR << "parseRequest failed!";
         conn->send("HTTP/1.1 400 Bad Request\r\n\r\n");
         conn->shutdown();
     }
@@ -73,7 +73,7 @@ void HttpServer::onMessage(const TcpConnectionPtr& conn,
     // 如果成功解析
     if (context->gotAll())
     {
-        LOG_INFO << "parseRequest success!";
+        // LOG_INFO << "parseRequest success!";
         onRequest(conn, context->request());
         context->reset();
     }
@@ -86,16 +86,22 @@ void HttpServer::onRequest(const TcpConnectionPtr& conn, const HttpRequest& req)
     // 判断长连接还是短连接
     bool close = connection == "close" ||
         (req.version() == HttpRequest::kHttp10 && connection != "Keep-Alive");
-    // TODO:这里有问题，但是强制改写了
+
+    // TODO:强制设的短链接
     close = true;
+
     // 响应信息
     HttpResponse response(close);
+
     // httpCallback_ 由用户传入，怎么写响应体由用户决定
     // 此处初始化了一些response的信息，比如响应码，回复OK
     httpCallback_(req, &response);
     Buffer buf;
     response.appendToBuffer(&buf);
+
     // TODO:需要重载 TcpConnection::send 使其可以接收一个缓冲区
+    // 长连接的情况
+    // 这只是一个例子，解析http也不用C++来写
     conn->send(buf.retriveAllAsString());
     if (response.closeConnection())
     {
